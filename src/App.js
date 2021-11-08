807e58e100a323fd205c02d49a663ab31a7aaa9d
Zain Shk <zainshk025@gmail.com>
2021-11-08 17:06:20 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header/Header';
+import List from './components/Expense List/List';
+import Inputs from './components/Transaction Inputs/Inputs';
 import './App.css';
+
+//import ContextAPI/ Provider
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <>
+ <GlobalProvider>
+      <Header/>
+      <List/>
+      <Inputs/>
+      </GlobalProvider>
+    </>
   );
 }
 
